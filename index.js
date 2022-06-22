25098f5b6c24e98ff6417063f1e70426cc030d25
enitanadata <empressbee0@gmail.com>
2022-06-22 10:39:11 -0700

    basic arithmetic calculator created with javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+const operator = prompt("Enter choice:+, -, /, *, %");
+const num1 = parseFloat(prompt("Enter first number: "));
+const num2 = parseFloat(prompt("Enter second number: "));
+
+
+let result;
+
+if (operator == '+') {
+    result = num1 + num2;
+
+} else if (operator == '-') {
+    result = num1 - num2;
+
+} else if (operator == '/') {
+    result = num1 / num2;
+
+} else if (operator == '*') {
+    result = num1 * num2;
+
+} else {
+    result = num1 % num2;
+
+}
+alert(result);
